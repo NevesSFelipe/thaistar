b80fe52458f34067464c842c32071008b17c1167
Felipe S. Neves <felipe.s.neves@outlook.com>
2025-06-23 01:33:46 -0300

    Corrigindo a variavel $table na model FuncionarioModel

--- src/Models/FuncionarioModel.php
+++ src/Models/FuncionarioModel.php
@@ -6,6 +6,14 @@ use App\Configs\Database;
 
 class FuncionarioModel extends Database {
 
+    private $tabelaFuncionarios;
+
+    public function __construct()
+    {
+        $this->tabelaFuncionarios = "horarios_atendimento";
+        parent::__construct(); // Chama o construtor de Database (responsável pela conexão)
+    }
+
     public function autenticarFuncionario($email)
     {
         $sql = "SELECT * FROM funcionarios WHERE email = ?";
